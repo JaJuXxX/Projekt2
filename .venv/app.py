f47fce728473ddeb35eda0cad1589654edea69f9
Kamil Jajuga <k.jajuga@joser-erbacher.de>
2022-12-18 15:49:47 +0100

    requirements

--- .venv/app.py
+++ .venv/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, jsonify
+# Importing Flask
+from flask import Flask, render_template
+from django.shortcuts import render
 
 # Creating an instance of the Flask class
 app = Flask(__name__)
@@ -6,9 +8,7 @@ app = Flask(__name__)
 # Creating a new endpoint with this instance as a function decorator
 @app.route('/')
 def hello_world():
-    return jsonify({
-        "message": "Hello World."
-    })
+    return render_template("index")
 
 # Starting the Server
 if __name__ == '__main__':
